f9f020ed313defae6f96f446e882ef407df09ff4
JP Simard <jp@jpsim.com>
2016-04-11 14:00:53 -0700

    add items to table view

--- RealmClear/ViewController.swift
+++ RealmClear/ViewController.swift
@@ -9,6 +9,23 @@
 import Cartography
 import UIKit
 
+var items =  [
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    "Quisque at magna auctor, rhoncus massa sit amet, sodales felis.",
+    "Suspendisse consequat purus at dolor ultricies interdum.",
+    "In luctus magna aliquet, tincidunt ante et, aliquam tellus.",
+    "Suspendisse suscipit lorem ac purus interdum, eu maximus ante pellentesque.",
+    "Aenean elementum est ut eros varius posuere vel sit amet eros.",
+    "Duis accumsan dolor quis leo tincidunt consectetur.",
+    "Proin dictum felis non dui dapibus molestie.",
+    "Fusce id est eget erat blandit rutrum.",
+    "Fusce rutrum ipsum ac nisi euismod pellentesque.",
+    "Nulla venenatis neque id eros consectetur, id pretium turpis sodales.",
+    "Nulla in sem pharetra, hendrerit diam ac, mollis nisl.",
+    "Nulla nec lectus sed massa tristique maximus.",
+    "Cras aliquam velit luctus lacus accumsan, id fringilla eros commodo."
+].map(ToDoItem.init)
+
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     let tableView = UITableView()
 
@@ -40,10 +57,12 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     // MARK: UITableViewDataSource
 
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return items.count
     }
 
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        return tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)
+        cell.textLabel?.text = items[indexPath.row].text
+        return cell
     }
 }
