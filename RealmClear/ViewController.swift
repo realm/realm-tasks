9f6785e827613f1c3720fc061b492377266cc35e
JP Simard <jp@jpsim.com>
2016-04-11 13:28:50 -0700

    Initial Commit

---
+++ RealmClear/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RealmClear
+//
+//  Created by JP Simard on 4/11/16.
+//  Copyright © 2016 Realm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
