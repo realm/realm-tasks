dcc4f115c6296983a6eddadb189631642dd22552
JP Simard <jp@jpsim.com>
2016-04-11 13:50:40 -0700

    add UITableView subview to ViewController

--- RealmClear/ViewController.swift
+++ RealmClear/ViewController.swift
@@ -6,7 +6,25 @@
 //  Copyright © 2016 Realm. All rights reserved.
 //
 
+import Cartography
 import UIKit
 
 class ViewController: UIViewController {
+    let tableView = UITableView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+
+    func setupUI() {
+        setupTableView()
+    }
+
+    func setupTableView() {
+        view.addSubview(tableView)
+        constrain(tableView) { tableView in
+            tableView.edges == tableView.superview!.edges
+        }
+    }
 }
