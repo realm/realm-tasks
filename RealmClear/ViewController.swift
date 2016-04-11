dcdc4e68f561f8ddf935e65dbca340b5414592ed
JP Simard <jp@jpsim.com>
2016-04-11 13:55:26 -0700

    further set up table view, stubbing out data source methods

--- RealmClear/ViewController.swift
+++ RealmClear/ViewController.swift
@@ -9,7 +9,7 @@
 import Cartography
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     let tableView = UITableView()
 
     override func viewDidLoad() {
@@ -26,5 +26,24 @@ class ViewController: UIViewController {
         constrain(tableView) { tableView in
             tableView.edges == tableView.superview!.edges
         }
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.separatorStyle = .None
+        tableView.backgroundColor = .blackColor()
+        tableView.rowHeight = 54
+        tableView.contentInset = UIEdgeInsets(top: 45, left: 0, bottom: 0, right: 0)
+        tableView.contentOffset = CGPoint(x: 0, y: -tableView.contentInset.top)
+        tableView.showsVerticalScrollIndicator = false
+    }
+
+    // MARK: UITableViewDataSource
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)
     }
 }
