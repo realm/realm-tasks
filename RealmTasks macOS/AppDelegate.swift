65597a4c48211e5f52627555d3df95b38c4a9d19
Dmitry Obukhov <stel2k@gmail.com>
2016-07-25 13:48:38 +0200

    add target for macOS app

---
+++ RealmTasks macOS/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  RealmTasks
+//
+//  Created by Dmitry Obukhov on 25/07/16.
+//  Copyright © 2016 Realm. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
