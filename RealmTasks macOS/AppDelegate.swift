090ef76a779537e325e3330581df44f18731068b
Dmitry Obukhov <stel2k@gmail.com>
2016-07-25 15:07:39 +0200

    initializing default realms

--- RealmTasks macOS/AppDelegate.swift
+++ RealmTasks macOS/AppDelegate.swift
@@ -1,27 +1,56 @@
-//
-//  AppDelegate.swift
-//  RealmTasks
-//
-//  Created by Dmitry Obukhov on 25/07/16.
-//  Copyright © 2016 Realm. All rights reserved.
-//
+/*************************************************************************
+ *
+ * REALM CONFIDENTIAL
+ * __________________
+ *
+ *  [2016] Realm Inc
+ *  All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Realm Incorporated and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Realm Incorporated
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Realm Incorporated.
+ *
+ **************************************************************************/
 
 import Cocoa
+import Realm
+import RealmSwift
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
 
-
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+        RLMSyncManager.sharedManager().configureWithAppID(Constants.appID)
+        
+        Realm.Configuration.defaultConfiguration = syncRealmConfiguration
+        Realm.setGlobalSynchronizationLoggingLevel(.Verbose)
+        
+        do {
+            let realm = try Realm()
+            if realm.isEmpty {
+                // Create a default list if none exist
+                try realm.write {
+                    realm.add(ToDoList())
+                }
+            }
+        } catch {
+            fatalError("Could not open or write to the realm: \(error)")
+        }
+        
+        if let userRealm = try? Realm(configuration: userRealmConfiguration),
+            let token = userRealm.objects(User.self).first?.accessToken {
+            try! Realm().open(with: token)
+        } else {
+            // TODO: log in
+        }
     }
-
-    func applicationWillTerminate(aNotification: NSNotification) {
-        // Insert code here to tear down your application
-    }
-
 
 }
-
