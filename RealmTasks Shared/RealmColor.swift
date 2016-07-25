779e857433b2671e71eb1982322421a933766d02
Dmitry Obukhov <stel2k@gmail.com>
2016-07-25 14:21:26 +0200

    fix RealmColor on OS X

--- RealmTasks Shared/RealmColor.swift
+++ RealmTasks Shared/RealmColor.swift
@@ -19,28 +19,37 @@
  **************************************************************************/
 
 import Foundation
-import UIKit
 
-extension UIColor {
+#if os(iOS)
+    import UIKit
+    
+    typealias Color = UIColor
+#elseif os(OSX)
+    import AppKit
+    
+    typealias Color = NSColor
+#endif
 
-    public var realmColors: [UIColor] {
-        return [UIColor(red: 231.0/255.0, green: 167.0/255.0, blue: 118.0/255.0, alpha: 1.0),
-                UIColor(red: 228.0/255.0, green: 125.0/255.0, blue: 114.0/255.0, alpha: 1.0),
-                UIColor(red: 233.0/255.0, green: 99.0/255.0, blue: 111.0/255.0, alpha: 1.0),
-                UIColor(red: 242.0/255.0, green: 81.0/255.0, blue: 145.0/255.0, alpha: 1.0),
-                UIColor(red: 154.0/255.0, green: 80.0/255.0, blue: 164.0/255.0, alpha: 1.0),
-                UIColor(red: 88.0/255.0, green: 86.0/255.0, blue: 157.0/255.0, alpha: 1.0),
-                UIColor(red: 56.0/255.0, green: 71.0/255.0, blue: 126.0/255.0, alpha: 1.0)]
+extension Color {
+
+    var realmColors: [Color] {
+        return [Color(red: 231.0/255.0, green: 167.0/255.0, blue: 118.0/255.0, alpha: 1.0),
+                Color(red: 228.0/255.0, green: 125.0/255.0, blue: 114.0/255.0, alpha: 1.0),
+                Color(red: 233.0/255.0, green: 99.0/255.0, blue: 111.0/255.0, alpha: 1.0),
+                Color(red: 242.0/255.0, green: 81.0/255.0, blue: 145.0/255.0, alpha: 1.0),
+                Color(red: 154.0/255.0, green: 80.0/255.0, blue: 164.0/255.0, alpha: 1.0),
+                Color(red: 88.0/255.0, green: 86.0/255.0, blue: 157.0/255.0, alpha: 1.0),
+                Color(red: 56.0/255.0, green: 71.0/255.0, blue: 126.0/255.0, alpha: 1.0)]
     }
 
-    public class func colorForRealmLogoGradient(offset: Double) -> UIColor {
+    class func colorForRealmLogoGradient(offset: Double) -> Color {
         var newOffset = offset
 
         // Ensure offset is normalized to 1.0
         newOffset = min(newOffset, 1.0)
         newOffset = max(newOffset, 0.0)
 
-        let realmLogoColors = UIColor().realmColors
+        let realmLogoColors = Color().realmColors
 
         // Work out the 'size' that each color stop spans
         let colorStopRange = 1.0 / Double(realmLogoColors.count-1)
@@ -67,6 +76,7 @@ extension UIColor {
         finalColors[1] = fromColors[1] + CGFloat(stopOffset) * (toColors[1] - fromColors[1])
         finalColors[2] = fromColors[2] + CGFloat(stopOffset) * (toColors[2] - fromColors[2])
 
-        return UIColor(red: finalColors[0], green: finalColors[1], blue: finalColors[2], alpha: 1.0)
+        return Color(red: finalColors[0], green: finalColors[1], blue: finalColors[2], alpha: 1.0)
     }
+
 }
