c4ef7b8273c46011d5f1775264dce2e2593ac93d
JP Simard <jp@jpsim.com>
2016-08-08 11:06:46 -0700

    layer title bar over all other views and animate title in and out

--- RealmTasks iOS/AppDelegate.swift
+++ RealmTasks iOS/AppDelegate.swift
@@ -18,13 +18,38 @@
  *
  **************************************************************************/
 
+import Cartography
 import UIKit
 import Realm
 import RealmSwift
 
 class ContainerViewController: UIViewController {
+    private var titleLabel = UILabel()
+    private var titleTopConstraint: NSLayoutConstraint?
+    override var title: String? {
+        didSet {
+            if let title = title {
+                titleLabel.text = title
+            }
+            titleTopConstraint?.constant = (title != nil) ? 20 : 0
+            UIView.animateWithDuration(0.2) {
+                self.titleLabel.alpha = (self.title != nil) ? 1 : 0
+                self.titleLabel.superview?.layoutIfNeeded()
+            }
+        }
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        addChildVC()
+        setupTitleBar()
+    }
+
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return .LightContent
+    }
+
+    private func addChildVC() {
         let firstList = try! Realm().objects(ToDoList.self).first!
         let vc = ViewController<ToDoItem, ToDoList>(
             items: firstList.items,
@@ -32,13 +57,34 @@ class ContainerViewController: UIViewController {
             title: firstList.name,
             getList: { $0.items }
         )
+        title = firstList.name
         addChildViewController(vc)
         view.addSubview(vc.view)
         vc.didMoveToParentViewController(self)
     }
 
-    override func preferredStatusBarStyle() -> UIStatusBarStyle {
-        return .LightContent
+    private func setupTitleBar() {
+        let titleBar = UIToolbar()
+        titleBar.barStyle = .BlackTranslucent
+        view.addSubview(titleBar)
+        constrain(titleBar) { titleBar in
+            titleBar.left == titleBar.superview!.left
+            titleBar.top == titleBar.superview!.top
+            titleBar.right == titleBar.superview!.right
+            titleBar.height >= 20
+            titleBar.height == 20 ~ UILayoutPriorityDefaultHigh
+        }
+
+        titleLabel.font = .boldSystemFontOfSize(13)
+        titleLabel.textAlignment = .Center
+        titleLabel.textColor = .whiteColor()
+        titleBar.addSubview(titleLabel)
+        constrain(titleLabel) { titleLabel in
+            titleLabel.left == titleLabel.superview!.left
+            titleLabel.right == titleLabel.superview!.right
+            titleLabel.bottom == titleLabel.superview!.bottom - 5
+            titleTopConstraint = (titleLabel.top == titleLabel.superview!.top + 20)
+        }
     }
 }
 
