90291b41a87e3e90200b4f303303b400a904ca89
JP Simard <jp@jpsim.com>
2016-08-08 16:20:47 -0700

    step 2: import Realm Swift & create models

--- MinimalTasks/ViewController.swift
+++ MinimalTasks/ViewController.swift
@@ -19,5 +19,25 @@
  **************************************************************************/
 
 import UIKit
+import RealmSwift
+
+// MARK: Models
+
+final class TaskList: Object {
+    dynamic var text = ""
+    dynamic var id = ""
+    let items = List<Task>()
+
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
+
+final class Task: Object {
+    dynamic var text = ""
+    dynamic var completed = false
+}
+
+// MARK: View Controller
 
 class ViewController: UITableViewController {}
