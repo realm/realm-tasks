a8fda9214caddb59e3c51054cd34f7923d460e37
Tim Oliver <me@timoliver.com.au>
2016-08-13 01:43:17 +0900

    Updated project file to include new files

--- RealmTasks iOS/ShareButtonView.swift
+++ RealmTasks iOS/ShareButtonView.swift
@@ -1,13 +1,63 @@
-//
-//  ShareButtonView.swift
-//  RealmTasks
-//
-//  Created by Tim Oliver on 11/08/2016.
-//  Copyright © 2016 Realm. All rights reserved.
-//
+/*************************************************************************
+ *
+ * REALM CONFIDENTIAL
+ * __________________
+ *
+ *  [2016] Realm Inc
+ *  All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Realm Incorporated and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Realm Incorporated
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Realm Incorporated.
+ *
+ **************************************************************************/
 
-import Cocoa
+import UIKit
+import Cartography
 
 class ShareButtonView: UIView {
 
+    private let shareButton = UIButton(type: .System)
+
+    var buttonTappedHandler: (() -> ())?
+
+    init() {
+        super.init(frame: CGRectMake(0,0,320,44))
+        self.backgroundColor = .blackColor()
+        self.tintColor = UIColor(white: 0.3, alpha: 1.0)
+        setupButton()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setupButton() {
+        let image = UIImage(named: "ShareIcon")?.imageWithRenderingMode(.AlwaysTemplate)
+        shareButton.setImage(image, forState: .Normal)
+        shareButton.setTitle("Share List", forState: .Normal)
+        shareButton.titleLabel!.font = UIFont.systemFontOfSize(18.0)
+        shareButton.addTarget(self, action: #selector(self.buttonTapped), forControlEvents: .TouchUpInside)
+        shareButton.sizeToFit()
+        shareButton.titleEdgeInsets = UIEdgeInsetsMake(0, 6, 0, -6)
+        shareButton.imageEdgeInsets = UIEdgeInsetsMake(-2, 0, 2, 0)
+
+        addSubview(shareButton)
+
+        constrain(shareButton) { shareButton in
+            shareButton.center == shareButton.superview!.center
+        }
+    }
+
+    func buttonTapped(sender: AnyObject?) {
+        if let buttonTappedHandler = buttonTappedHandler {
+            buttonTappedHandler()
+        }
+    }
 }
