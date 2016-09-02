a6ec192da36e42a5a003c12fac62719732422e66
JP Simard <jp@jpsim.com>
2016-09-02 15:10:24 -0700

    fix legacy_constructor violations

--- RealmTasks iOS/CellTextView.swift
+++ RealmTasks iOS/CellTextView.swift
@@ -45,7 +45,7 @@ final class CellTextView: UITextView {
 
     func strike(fraction: Double = 1) {
         let mutableAttributedString = attributedText!.mutableCopy() as! NSMutableAttributedString
-        mutableAttributedString.addAttribute(NSStrikethroughStyleAttributeName, value: 2, range: NSMakeRange(0, Int(fraction * Double(mutableAttributedString.length))))
+        mutableAttributedString.addAttribute(NSStrikethroughStyleAttributeName, value: 2, range: NSRange(location: 0, length: Int(fraction * Double(mutableAttributedString.length))))
         attributedText = mutableAttributedString.copy() as? NSAttributedString
     }
 
@@ -54,7 +54,7 @@ final class CellTextView: UITextView {
         mutableTypingAttributes.removeValueForKey(NSStrikethroughStyleAttributeName)
         typingAttributes = mutableTypingAttributes
         let mutableAttributedString = attributedText!.mutableCopy() as! NSMutableAttributedString
-        mutableAttributedString.removeAttribute(NSStrikethroughStyleAttributeName, range: NSMakeRange(0, mutableAttributedString.length))
+        mutableAttributedString.removeAttribute(NSStrikethroughStyleAttributeName, range: NSRange(location: 0, length: mutableAttributedString.length))
         attributedText = mutableAttributedString.copy() as? NSAttributedString
     }
 }
