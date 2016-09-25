4b28a66eb4ff585ffd67903945eafba1d8fba5ce
Radu Tutueanu <rt@realm.io>
2016-09-25 18:59:22 +0200

    Delete dev team and provisioning profile

--- package_release.rb
+++ package_release.rb
@@ -76,5 +76,5 @@ embed_realm_frameworks('osx', 'macOS')
 # 4. Build RealmTasks macOS
 ################################################################
 
-`xcodebuild -workspace RealmTasks.xcworkspace -scheme "RealmTasks macOS" -derivedDataPath 'build' DEVELOPMENT_TEAM=QX5CR2FTN2 CODE_SIGN_IDENTITY='Developer ID Application' clean build -configuration Debug PROVISIONING_PROFILE=42568216-dcbb-4f07-b5ef-cda9ddfaa31e`
+`xcodebuild -workspace RealmTasks.xcworkspace -scheme "RealmTasks macOS" -derivedDataPath 'build' CODE_SIGN_IDENTITY='Developer ID Application' clean build -configuration Debug`
 `mv "build/Build/Products/Debug/RealmTasks.app" .`
