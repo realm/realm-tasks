c3c4211b231cb5efb6894e9dfcf9015529f8a16c
JP Simard <jp@jpsim.com>
2016-09-25 11:04:09 -0700

    re-sign the app after building

--- package_release.rb
+++ package_release.rb
@@ -78,3 +78,4 @@ embed_realm_frameworks('osx', 'macOS')
 
 `xcodebuild -workspace RealmTasks.xcworkspace -scheme "RealmTasks macOS" -derivedDataPath 'build' DEVELOPMENT_TEAM=QX5CR2FTN2 clean build -configuration Debug PROVISIONING_PROFILE=42568216-dcbb-4f07-b5ef-cda9ddfaa31e`
 `mv "build/Build/Products/Debug/RealmTasks.app" .`
+`codesign --force --sign "Developer ID Application: Realm ApS" RealmTasks.app`
