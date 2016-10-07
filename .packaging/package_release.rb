ada869efa046d17e8cd8cc584bf206a0ea34ff05
Dmitry Obukhov <stel2k@gmail.com>
2016-10-07 17:08:35 +0200

    reset PROVISIONING_PROFILE_SPECIFIER option

--- .packaging/package_release.rb
+++ .packaging/package_release.rb
@@ -76,5 +76,5 @@ embed_realm_frameworks('osx', 'macOS')
 # 4. Build RealmTasks macOS
 ################################################################
 
-`xcodebuild -workspace "RealmTasks Apple/RealmTasks.xcworkspace" -scheme "RealmTasks macOS" -derivedDataPath "build" DEVELOPMENT_TEAM=QX5CR2FTN2 CODE_SIGN_IDENTITY="Developer ID Application" clean build`
-`mv "build/Build/Products/Debug/RealmTasks.app" .`
+`xcodebuild -workspace 'RealmTasks Apple/RealmTasks.xcworkspace' -scheme 'RealmTasks macOS' -derivedDataPath 'build' DEVELOPMENT_TEAM='QX5CR2FTN2'' CODE_SIGN_IDENTITY='Developer ID Application' PROVISIONING_PROFILE_SPECIFIER='' clean build`
+`mv 'build/Build/Products/Debug/RealmTasks.app' .`
