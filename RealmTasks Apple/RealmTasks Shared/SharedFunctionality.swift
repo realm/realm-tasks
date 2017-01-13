3620f19e9ef4f0f97a3a3346a6e4f482cd9429d0
JP Simard <jp@jpsim.com>
2017-01-12 18:11:05 -0800

    update deduplication async write to use ThreadSafeReference API

--- RealmTasks Apple/RealmTasks Shared/SharedFunctionality.swift
+++ RealmTasks Apple/RealmTasks Shared/SharedFunctionality.swift
@@ -44,24 +44,25 @@ private func setDefaultRealmConfiguration(with user: SyncUser) {
 
     // FIXME: Remove once core supports ordered sets: https://github.com/realm/realm-core/issues/1206
     deduplicationNotificationToken = realm.addNotificationBlock { _, realm in
-        guard realm.objects(TaskListList.self).first!.items.count > 1 else {
-            return
-        }
+        let items = realm.objects(TaskListList.self).first!.items
+        guard items.count > 1 && !realm.isInWriteTransaction else { return }
+        let itemsReference = ThreadSafeReference(to: items)
         // Deduplicate
-        DispatchQueue(label: "io.realm.RealmTasks.bg", attributes: []).async {
-            var items = try! Realm().objects(TaskListList.self).first!.items
-            guard items.count > 1 else { return }
-
-            try! items.realm!.write {
-                let listReferenceIDs = NSCountedSet(array: items.map { $0.id })
-                for id in listReferenceIDs where listReferenceIDs.count(for: id) > 1 {
-                    let id = id as! String
-                    let indexesToRemove = items.enumerated().flatMap { index, element in
-                        return element.id == id ? index : nil
-                    }
-                    indexesToRemove.dropFirst().reversed().forEach { items.remove(at: $0) }
-                }
+        DispatchQueue(label: "io.realm.RealmTasks.bg").async {
+            let realm = try! Realm()
+            guard let items = realm.resolve(itemsReference), items.count > 1 else {
+                return
             }
+            realm.beginWrite()
+            let listReferenceIDs = NSCountedSet(array: items.map { $0.id })
+            for id in listReferenceIDs where listReferenceIDs.count(for: id) > 1 {
+                let id = id as! String
+                let indexesToRemove = items.enumerated().flatMap { index, element in
+                    return element.id == id ? index : nil
+                }
+                indexesToRemove.dropFirst().reversed().forEach(items.remove(objectAtIndex:))
+            }
+            try! realm.commitWrite()
         }
     }
 }
