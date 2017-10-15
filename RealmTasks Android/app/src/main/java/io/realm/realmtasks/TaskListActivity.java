5c3818d03d3f0c9af9083c0fea9094794162e7b3
Christian Melchior <christian@ilios.dk>
2017-10-15 13:52:35 +0200

    Use fine-grained notifications

--- RealmTasks Android/app/src/main/java/io/realm/realmtasks/TaskListActivity.java
+++ RealmTasks Android/app/src/main/java/io/realm/realmtasks/TaskListActivity.java
@@ -38,6 +38,9 @@ import io.realm.realmtasks.model.TaskList;
 import io.realm.realmtasks.model.TaskListList;
 import io.realm.realmtasks.view.RecyclerViewWithEmptyViewSupport;
 
+/**
+ * Show all lists.
+ */
 public class TaskListActivity extends AppCompatActivity {
 
     private Realm realm;
@@ -157,20 +160,17 @@ public class TaskListActivity extends AppCompatActivity {
             final int fromPosition = from.getAdapterPosition();
             final int toPosition = to.getAdapterPosition();
             adapter.onItemMoved(fromPosition, toPosition);
-            adapter.notifyItemMoved(fromPosition, toPosition);
         }
 
         @Override
         public void onCompleted(ItemViewHolder viewHolder) {
             adapter.onItemCompleted(viewHolder.getAdapterPosition());
-            adapter.notifyDataSetChanged();
         }
 
         @Override
         public void onDismissed(ItemViewHolder viewHolder) {
             final int position = viewHolder.getAdapterPosition();
             adapter.onItemDismissed(position);
-            adapter.notifyItemRemoved(position);
         }
 
         @Override
@@ -192,21 +192,16 @@ public class TaskListActivity extends AppCompatActivity {
         @Override
         public void onChanged(ItemViewHolder viewHolder) {
             adapter.onItemChanged(viewHolder);
-            adapter.notifyItemChanged(viewHolder.getAdapterPosition());
         }
 
         @Override
         public void onAdded() {
             adapter.onItemAdded();
-            adapter.notifyItemInserted(0);
         }
 
         @Override
         public void onReverted(boolean shouldUpdateUI) {
             adapter.onItemReverted();
-            if (shouldUpdateUI) {
-                adapter.notifyDataSetChanged();
-            }
         }
 
         @Override
